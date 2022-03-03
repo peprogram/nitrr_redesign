8b10a61df7196c81dae987bf703e806b80fbe7ce
peprogram <73199822+peprogram@users.noreply.github.com>
2022-03-03 20:08:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
     document.getElementById("fix").style.padding = "0";
+    document.getElementById("fix").style.marginBottom = "-10px";
     document.getElementById("head").style.position = "fixed";
     document.getElementById("main").style.marginTop = "30vh";
     if (screen.width<=560)
@@ -10,6 +11,7 @@ function scrollFunction() {
   } else {
     document.getElementById("head").style.position = "static";
     document.getElementById("fix").style.padding = "10px 0";
+    document.getElementById("fix").style.marginBottom = "0px";
     document.getElementById("main").style.marginTop = "0px";
     document.getElementById("hb").style.display = "block";
   }
